a0f510be25e1486dddb4825bad3ec77186f6c0b5
elijahanderson <elijah.anderson.18@cnu.edu>
2022-02-03 23:16:11 -0500

    Created Some Sample Data

--- Database_Creation.sql
+++ Database_Creation.sql
@@ -9,7 +9,7 @@ CREATE TABLE Users
     fName  VARCHAR(10)               NOT NULL,
     lName  VARCHAR(15)               NOT NULL,
     email  VARCHAR(50)               NOT NULL,
-    phone  INT                       NOT NULL,
+    phone  VARCHAR(10)                       NOT NULL,
     Constraint user_pk
         PRIMARY KEY (userID)
 );
@@ -19,7 +19,7 @@ CREATE TABLE Inventory
 (
     itemID         INT AUTO_INCREMENT NOT NULL,
     productName    VARCHAR(50)        NOT NULL,
-    warrantyStatus VARCHAR(20),
+    warrantyStatus BOOLEAN,
     CONSTRAINT itemID_pk
         PRIMARY KEY (itemID)
 );
@@ -40,3 +40,21 @@ CREATE TABLE Log
     FOREIGN KEY (itemID) REFERENCES Inventory (itemID),
     FOREIGN KEY (userID) REFERENCES Users (userID)
 );
+
+INSERT INTO Users
+VALUES (00001, 'Elijah', 'Anderson', 'elijah.anderson.18@cnu.edu', '7577084183');
+INSERT INTO Users
+VALUES (00002, 'Captain', 'Chris', 'captain.chris@cnu.edu', '7578675309');
+INSERT INTO Users
+VALUES (00003, 'Chris', 'Kreider', 'chris.kreider@cnu.edu', '8005882300');
+
+INSERT INTO Inventory
+VALUES (00001, 'Canon 90D', TRUE);
+INSERT INTO Inventory
+VALUES (00002, 'Behringer Xenyx 802USB', FALSE);
+INSERT INTO Inventory
+VALUES (00003, 'Behringer B85', False);
+
+INSERT INTO Log
+VALUES (000001, 00002, 'Canon 90D', '2022-02-02', 00001, 'Elijah', 'Anderson', 'elijah.anderson.18@cnu.edu',
+        '7577084183');
