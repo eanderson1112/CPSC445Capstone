2b2fa952d4f156d693a4681287251bec55975422
elijahanderson <elijah.anderson.18@cnu.edu>
2022-04-01 10:01:21 -0400

    Did stuff

--- database_connection.php
+++ database_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost:3306";
+$servername = "imscapstone.mysql.database.azure.com";
 $database = "Inventory";
-$username = "root";
-$password = "Beagle26!";
+$username = "CPSC445Student@imscapstone";
+$password = "Troop7gator!";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -10,6 +10,4 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-//echo "Connected successfully";
-
+echo "Connected successfully";
